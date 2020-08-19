e35f00b9b07e9878e197d94f0dceeedddaec5179
khulitshiks <tshikalangek@gmail.com>
2020-08-20 01:06:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-challange3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-challange3)
---
+++ index.ts
@@ -0,0 +1,9 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+
+console.log(Math.min(13,6));
